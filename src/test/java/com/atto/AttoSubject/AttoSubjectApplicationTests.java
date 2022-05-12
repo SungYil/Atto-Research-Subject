3eb7e80d64efe6f0931fdbed2d59bc82a23e383a
ParkSungYil <shfur2006@gmail.com>
2022-05-13 01:37:42 +0900

    project init

---
+++ src/test/java/com/atto/AttoSubject/AttoSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atto.AttoSubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttoSubjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
