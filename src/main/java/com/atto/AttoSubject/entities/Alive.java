27948963297346670abfd0b191391a5d4a745339
ParkSungYil <shfur2006@gmail.com>
2022-05-13 16:30:04 +0900

    Host crud를 위한 엔티티 작성

---
+++ src/main/java/com/atto/AttoSubject/entities/Alive.java
@@ -0,0 +1,22 @@
+package com.atto.AttoSubject.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@Entity
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Alive extends BaseEntity{
+    @OneToOne
+    @JoinColumn(name = "ip")
+    private Host host;
+    private Enum state;
+    private ZonedDateTime checkTime;
+}
---
+++ src/main/java/com/atto/AttoSubject/entities/Host.java
@@ -0,0 +1,19 @@
+package com.atto.AttoSubject.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@Entity
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class Host extends BaseEntity{
+    private String ip;
+    private String name;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime updatedAt;
+}
